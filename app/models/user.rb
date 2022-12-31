0107a0c1f3c2cc3a874e2d967d3a6be2f93792dc
sea-yassan33 <a.yasuda_tr51@me.com>
2022-12-31 16:02:29 +0900

    新規登録フォーム

---
+++ app/models/user.rb
@@ -0,0 +1,13 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  with_options presence: true do
+    validates :password,
+              format: { with: /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i,
+                        message: 'is invalid. Include both letters and numbers' }
+    validates :nickname
+  end
+end
